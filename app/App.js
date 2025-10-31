c60dcaf62811366a21816a4e962508d9219a299b
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-31 14:00:15 -0300

    chore: initial setup

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+ return (
+   <View>
+       <Text>Initialize project</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
