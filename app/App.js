864e505f267875c5d8ae297eeaad145df0f4f3ed
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-31 14:49:31 -0300

    feat: searching for API currencies

--- app/App.js
+++ app/App.js
@@ -1,7 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { PickerItem } from '../src/components/Picker';
+import { api } from '../src/services/api';
 
 export default function App() {
+
+  const [moedas, setMoedas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadMoedas() {
+      const response = await api.get('all')
+      let arrayMoedas = [];
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key,
+        })
+      })
+
+      setMoedas(arrayMoedas)
+      setLoading(false)
+
+    }
+
+    loadMoedas();
+  }, []);
+
+  if(loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',  backgroundColor: '#1D1D33' }}>
+        <ActivityIndicator color="#FFF" size="large"/>
+      </View>
+    )
+  }
+
  return (
    <View style={styles.container}>
       <View style={styles.areaMoeda}>
